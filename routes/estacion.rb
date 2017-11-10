508e38e029905d2cced50d15f791ddd5050515e8
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-09 20:30:04 -0500

    Listar estaciones, latitud y longitud como float

--- routes/estacion.rb
+++ routes/estacion.rb
@@ -1,5 +1,11 @@
 class MyApp < Sinatra::Base
-    get '/estacion/listar' do
-        Estacion.all.to_a.to_json
-    end
+		get '/estacion/listar' do
+				rs = Estacion.select(:ide_estacion, :latitud, :longitud)
+				rpta = []
+				for r in rs
+						temp = {:id => r[:ide_estacion], :latitud => r[:latitud].to_f, :longitud => r[:longitud].to_f}
+						rpta.push(temp)
+				end
+				rpta.to_json
+		end
 end
