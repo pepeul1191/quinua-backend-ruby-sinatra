621b2c8d43971515355a217f52b532dfb4595d56
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-10 09:51:42 -0500

    Agregando comentarios a config/database.rb

--- config/database.rb
+++ config/database.rb
@@ -3,5 +3,5 @@ require 'sqlite3'
 
 Sequel::Model.plugin :json_serializer
 
-#DB = Sequel.connect('postgres://postgres:ulima@172.16.28.184:5432/quinua')
-DB = Sequel.connect('sqlite://db/db_quinua.db')
+#DB = Sequel.connect('postgres://postgres:ulima@172.16.28.184:5432/quinua') # db en la red interna de IDIC
+DB = Sequel.connect('sqlite://db/db_quinua.db') # db local de pruebas
