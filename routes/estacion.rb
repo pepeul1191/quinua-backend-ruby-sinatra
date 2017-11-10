3ca6f33a5c3dff3a9f71f049483b55008fb75784
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-09 20:38:36 -0500

    Los cambios del commit anterior no eran necesarios XD

--- routes/estacion.rb
+++ routes/estacion.rb
@@ -1,11 +1,5 @@
 class MyApp < Sinatra::Base
 		get '/estacion/listar' do
-				rs = Estacion.select(:ide_estacion, :latitud, :longitud)
-				rpta = []
-				for r in rs
-						temp = {:id => r[:ide_estacion], :latitud => r[:latitud].to_f, :longitud => r[:longitud].to_f}
-						rpta.push(temp)
-				end
-				rpta.to_json
+				Estacion.select(:ide_estacion, :latitud, :longitud).to_json
 		end
 end
